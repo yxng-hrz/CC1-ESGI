8030d98bb6992204e577a189e0035e68bc811b3c
Guillaume Houriez <guillaume.houriez@epitech.eu>
2024-09-27 10:46:33 +0200

    [ADD] : the main function who run all principal functions

--- src/main.c
+++ src/main.c
@@ -6,3 +6,19 @@
 
 #include "tableline.h"
 #include "binarytree.h"
+#include "sqlhandler.h"
+
+int main()
+{
+    Table* table = create_table();
+    Row row1 = {1, "AZERT"};
+    Row row2 = {2, "YUIOP"};
+    insert_row(table, row1);
+    insert_row(table, row2);
+    Node* root = 0;
+    root = insert(root, row1);
+    root = insert(root, row2);
+    free_table(table);
+    free_btree(root);
+    return NULL;
+}
